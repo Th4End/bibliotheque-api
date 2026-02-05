b2e86ba4424fb9b08905411cd5b3e32bc06294a8
Th4End <eliazidb@gmail.com>
2026-02-05 19:55:12 +0100

    chore: remove docker-compose file, move of the file

--- app/database.py
+++ app/database.py
@@ -11,5 +11,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
+try:
+    with engine.connect() as connection:
+        print("Database connected successfully.")
+except Exception as e:
+    print("Failed to connect to the database:", e)
+
 class Base(DeclarativeBase):
     pass
---
+++ app/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from app.database import engine, Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
