3f782876cb9418e93e35eacdec4f89c26cb99b02
Th4End <eliazidb@gmail.com>
2026-02-08 15:57:25 +0100

    fix: remove duplicate import statements in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.database import engine, Base
+from app.models import Books, tags, users
+from sqlalchemy import inspect
+from app.database import engine
 
 app = FastAPI()
-
 Base.metadata.create_all(bind=engine)
