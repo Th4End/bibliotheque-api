731b3b0a327f97a0c2abea45f7b537e51d9be545
Th4End <eliazidb@gmail.com>
2026-02-13 21:38:01 +0100

    add: pydantic schema for Books, tags, users

---
+++ app/schemas/Books.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+class Bookcreate(BaseModel):
+    title: str
+    author: str
+    isbn: str | None = None
+    year: int | None = None
+
+class Bookupdate(BaseModel):
+    title: str | None = None
+    author: str | None = None
+    isbn: str | None = None
+    year: int | None = None
+
+class BookResponse(BaseModel):
+    id: int
+    title: str
+    author: str
+    isbn: str | None
+    year: int | None
