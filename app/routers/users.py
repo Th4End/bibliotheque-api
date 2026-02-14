9c3afa249f8043f2141d51abe99af86a16079f86
Th4End <eliazidb@gmail.com>
2026-02-14 21:34:54 +0100

    add: auth and security system for tags and users

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.users import User
+from app.core.auth import get_current_user
 
 router = APIRouter(
     prefix="/users",
@@ -14,7 +15,7 @@ def get_users(db: Session = Depends(get_db)):
     return db.query(User).all()
 
 @router.post("/", response_model=User, status_code=201)
-def create_user(user: User, db: Session = Depends(get_db)):
+def create_user(user: User, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     db_user = User(**user.dict())
     db.add(db_user)
     db.commit()
@@ -22,7 +23,7 @@ def create_user(user: User, db: Session = Depends(get_db)):
     return db_user
 
 @router.put("/{user_id}", response_model=User, status_code=200)
-def update_user(user_id: int, user: User, db: Session = Depends(get_db)):
+def update_user(user_id: int, user: User, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     db_user = db.query(User).filter(User.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -33,7 +34,7 @@ def update_user(user_id: int, user: User, db: Session = Depends(get_db)):
     return db_user
 
 @router.delete("/{user_id}", status_code=204)
-def delete_user(user_id: int, db: Session = Depends(get_db)):
+def delete_user(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     db_user = db.query(User).filter(User.id == user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
