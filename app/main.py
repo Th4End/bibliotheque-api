7438fe8452bf7d1a8a7fe1dd4e899d8fd532f265
Th4End <eliazidb@gmail.com>
2026-02-25 12:24:19 +0100

    add: cors policies

--- app/main.py
+++ app/main.py
@@ -3,10 +3,18 @@ from app.core.init_admin import create_default_admin
 from app.database import Base, engine
 from app.routers import admin, auth, books, tags
 from app.routers import search
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+    allow_credentials=True
+)
 
 app.include_router(books.router)
 app.include_router(tags.router)
